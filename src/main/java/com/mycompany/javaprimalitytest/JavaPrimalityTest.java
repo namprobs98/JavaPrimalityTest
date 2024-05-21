db0a80c255b81add69b97b3730808e667488c7bd
Slimy Nightmare <namprobs98@gmail.com>
2024-05-21 10:33:32 +0700

    done

---
+++ src/main/java/com/mycompany/javaprimalitytest/JavaPrimalityTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaprimalitytest;
+
+/**
+ *
+ * @author Nhat Anh
+ */
+public class JavaPrimalityTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
